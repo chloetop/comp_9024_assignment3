0c19ff01be5b12135571ffcfb2c539f358d948da
whj_nudt@foxmail.com <whj_nudt@foxmail.com>
2016-05-05 02:24:44 +1000

    finish the design of algorithm.

---
+++ src/net/datastructures/TaskScheduler.java
@@ -0,0 +1,25 @@
+package net.datastructures;
+/*
+ *Comp9024_Assignment3
+ *Author: Huijun Wu(z5055605)
+ *Date:04/05/2016 
+ */
+
+import net.datastructures.*;
+import java.util.*;
+import java.io.*;
+
+
+public class TaskScheduler{
+	
+	static void scheduler(String file1, String file2){
+		try{
+			Scanner file1_scanner = new Scanner(file1);
+		}catch(FileNotFoundException e){
+			System.out.println("FIle not found");
+		}
+		
+
+		
+	}
+}
