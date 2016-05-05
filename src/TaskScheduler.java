bc3e720e23094f35243155d21041eab91b44188d
whj_nudt@foxmail.com <whj_nudt@foxmail.com>
2016-05-05 14:55:30 +1000

    finish first version (just worked)

--- src/TaskScheduler.java
+++ src/TaskScheduler.java
@@ -11,7 +11,6 @@ import java.util.regex.Matcher;
 import java.io.*;
 import java.lang.*;
 
-
 class Task_Tuple{
 	 Integer rd_time;   //this rd_time could either be the release time or deadline time. 
 	 String task_name;
@@ -33,7 +32,7 @@ public class TaskScheduler{
 	static HeapPriorityQueue<Integer,Task_Tuple> task_queue = new HeapPriorityQueue<Integer,Task_Tuple>();
 	
 	static HeapPriorityQueue<Integer,Task_Tuple> schedule_queue = new HeapPriorityQueue<Integer,Task_Tuple>();
-	
+	//static String write_buffer_string = new String("");
 	//m is the number of cpu cores. 
 	static void scheduler(String file1, String file2, int m){
 		Scanner file1_scanner;
@@ -93,20 +92,25 @@ public class TaskScheduler{
 		Entry<Integer, Task_Tuple> task_tmp = task_queue.removeMin();
 		//secondly, we schedule the tasks with time sequence. 
 		schedule_queue.insert(task_tmp.getValue().rd_time, new Task_Tuple(task_tmp.getKey(), task_tmp.getValue().task_name));
+
+
+
+		//StringBuffer write_buffer = new StringBuffer("");
 		
-		//the default logic of file write is to cover the old file. 
-		try {
-	         BufferedWriter out_file = new BufferedWriter(new FileWriter(file2));
-			} catch (IOException e) {
-				System.out.println("File create failed.");
-			}
-		
-		
+		BufferedWriter out_file =  null;
+		try{
+			out_file = new BufferedWriter(new FileWriter(file2));}
+		catch(IOException e)
+		{
+			e.printStackTrace();
+		}
+		String write_buffer_string = new String("");
 		int cur_time = 0;
 		while((!task_queue.isEmpty())||(!schedule_queue.isEmpty())){
 			while((!task_queue.isEmpty())&&(task_queue.min().getKey()==cur_time))
 			{	
 				Entry<Integer, Task_Tuple> cur_task = task_queue.removeMin();
+
 				schedule_queue.insert(cur_task.getValue().rd_time, new Task_Tuple(cur_task.getKey(), cur_task.getValue().task_name));
 			}
 			for(int j = 0 ; j < m; j++)
@@ -117,43 +121,44 @@ public class TaskScheduler{
 					if(tmp_sche_result.getKey() <= cur_time)
 					{
 						System.out.println("No feasible schedule exists");
+						write_buffer_string = "No feasible schedule exists";
+						try{
+							out_file.write(write_buffer_string);
+
+							out_file.close();
+						}catch(IOException e)
+						{
+							System.out.println("IO exception");
+							e.printStackTrace();
+						}
+
 						return;
 					}
 					else{
-					System.out.printf("task for time %d is %s\n", cur_time, tmp_sche_result.getValue().task_name);
+						//write_buffer.append(tmp_sche_result.getValue().task_name).append(" ").append(cur_time).append(" ");
+						System.out.printf("task for time %d is %s\n", cur_time, tmp_sche_result.getValue().task_name);
+						write_buffer_string = write_buffer_string + tmp_sche_result.getValue().task_name + " " + cur_time + " ";
+						
 					}
 				}
 			}
 			cur_time++;
 		}
-		
-
+		try{
+			out_file.write(write_buffer_string);
+			out_file.close();
+		}catch(IOException e)
+		{
+			System.out.println("IO exception");
+			e.printStackTrace();
+		}
+		System.out.println(write_buffer_string);
 	}
 	
 	public static void main(String[] args)
 	{
 		
-		TaskScheduler.scheduler("/home/wuhuijun/java_workspace/comp9024_assignment3/src/file1.txt", "file2.txt", 2);
-	/*	String task_name_pattern = "([a-z])([0-9])*";
-		String file = "v1 0 2 v2 0 3 v3 1 2 v4 1 2 v5 1 3 v6 3 4 v7 3 4 v8 3 4 v9 3 4 v10 4 6  v11 6 7 v12 6 7 v13 7 8 v14 7 8 v15 7 9 v16 7 10";
-		Scanner s = new Scanner(file);
-		
-		String r_time_pattern = "(\\d)(\\d)*";
-		String d_time_pattern = "([1-9])(\\d)*";
-		Pattern file1_p = Pattern.compile(task_name_pattern);
-		Pattern r_time = Pattern.compile(r_time_pattern);
-		Pattern d_time = Pattern.compile(d_time_pattern);
-		try{
-			
-			String next = s.next(task_name_pattern);
-			System.out.println(next);
-			next = s.next(r_time);
-			System.out.println(next);
-		}catch(NoSuchElementException e)
-		{
-			System.out.println("error");
-		}
-		*/
+		TaskScheduler.scheduler("/home/wuhuijun/java_workspace/comp9024_assignment3/src/file1.txt", "/home/wuhuijun/java_workspace/comp9024_assignment3/src/file2.txt", 2);
 		
 	}
 }
